9450ffed430d2b82582d62fc98d0c5ac220788b7
jarek <39216951+jarek7410@users.noreply.github.com>
2023-05-17 13:13:30 +0200

    CardConfig update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import "./App.css";
 import {RenderCard} from "./Components/RenderCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import CardConfig from "./Resources/CardConfig"
 import data from "./Resources/CardConfig.json"
 
 export default function App() {
-    // console.log("data",data)
-    // console.log("carddata",CardConfig)
     const Card=[data];
   return (
       <div className="App">
